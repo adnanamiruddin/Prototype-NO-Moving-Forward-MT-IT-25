d65166f7ce1b4de6aee7c755310d75166199fbc4
adnanamiruddin <adnan.amiruddin34@gmail.com>
2026-08-02 08:40:07 +0700

    feat: add new icon

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -5,6 +5,12 @@ export default function MainLayout({ children }) {
     <>
       <Head>
         <title>Prototype NO - Moving Forward</title>
+
+        {/* Favicon */}
+        <link rel="icon" href="/web-icon.png" />
+
+        {/* For Apple Ecosystem */}
+        <link rel="apple-touch-icon" href="/favicon.png" />
       </Head>
 
       <div className="min-h-screen bg-gray-100 text-black">{children}</div>
